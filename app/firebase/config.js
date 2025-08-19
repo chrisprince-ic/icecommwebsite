1ffb0979d8b65342876b8ce980d4d36f97a14858
chris-prince <icprincechris@gmail.com>
2025-08-19 07:35:17 -0400

    changed some config data

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -8,12 +8,12 @@ import { getStorage } from "firebase/storage";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  authDomain: "ecommproject-ea39b.firebaseapp.com",
+  projectId: "ecommproject-ea39b",
+  storageBucket: "ecommproject-ea39b.firebasestorage.app",
+  messagingSenderId: "939303065850",
+  appId: "1:939303065850:web:0622c973a686644c50d143",
+  measurementId: "G-M13P0LC716",
 };
 
 export default firebaseConfig;
